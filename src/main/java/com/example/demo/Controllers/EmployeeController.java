91ef6da563c683704023ad1b0faa514d0cb4a91c
Oumaima <oumaima@Oumaimas-MacBook-Pro.local>
2023-12-21 15:26:36 +0100

    feat(EmployeeRequest,FetchEmployeeResponse,EmployeeMapper, EmployeeController):Add EmployeeRequest meant to encapsulate the rrequest  when fetching employees, add FetchEmployeeResponse  meant to encapsulate the response when fetching employees, add EmployeeMapper responsible for mapping a list of Employee objects to a FetchEmployeeResponse object,update  EmployeeController by adding fetch controller which handle all find employees by keywork employee business logic)

--- src/main/java/com/example/demo/Controllers/EmployeeController.java
+++ src/main/java/com/example/demo/Controllers/EmployeeController.java
@@ -1,8 +1,12 @@
 package com.example.demo.Controllers;
 
+import com.example.demo.Controllers.Mappers.EmployeeMapper;
+import com.example.demo.Controllers.Request.EmployeeRequest;
+import com.example.demo.Controllers.Response.FetchEmployeeResponse;
 import com.example.demo.Entities.Employee;
 import com.example.demo.ServicesImpl.EmployeeServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,8 +15,10 @@ import java.util.List;
 public class EmployeeController {
     @Autowired
     private EmployeeServiceImpl employeeServiceImpl;
+    @Autowired
+    private EmployeeMapper employeeMapper;
 
-    @GetMapping("/employees")
+    // @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return employeeServiceImpl.getAllEmployees();
     }
@@ -22,12 +28,31 @@ public class EmployeeController {
      * @param keyword
      * @return
      */
-    @GetMapping("/searchEmployees")
+    @GetMapping("/employees")
     public List<Employee> searchEmployees(@RequestParam(required = false) String keyword) {
         if(keyword==null){
-           return  List.of();
+            return  List.of();
         }
         return employeeServiceImpl.searchEmployees(keyword);
     }
+    /*
+     * @param keyword
+     * @return
+     */
+    @GetMapping(value="/fetch", consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public FetchEmployeeResponse fetchEmployees(@RequestBody EmployeeRequest employeeRequest) {
+        final String keyword=employeeRequest.getKeyword();
 
+        if(keyword==null){
+            // new FetchEmployeeResponse(List.of());
+            return employeeMapper.toFetchEmployeeResponse(List.of());
+            //return "Hello";
+        }
+        final  List<Employee> result=employeeServiceImpl.searchEmployees(keyword);
+        // return new FetchEmployeeResponse(result);
+        return  employeeMapper.toFetchEmployeeResponse(result);
+        //return "salut";
+
+    }
 }
