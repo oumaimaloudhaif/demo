8d69895f468cb2d28eb388b0360b8b8dd7c9747a
Oumaima <oumaima@Oumaimas-MacBook-Pro.local>
2023-12-29 21:34:09 +0100

    feat(EmployeeController,EmployeeRequest):Adding validation logic to both the EmployeeController and EmployeeRequest involves ensuring that the data received or processed adheres to specific criteria before handling it

--- src/main/java/com/example/demo/Controllers/Request/EmployeeRequest.java
+++ src/main/java/com/example/demo/Controllers/Request/EmployeeRequest.java
@@ -1,13 +1,22 @@
 package com.example.demo.Controllers.Request;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class EmployeeRequest {
+   @NotNull
+   @NotEmpty(message = "The keyword MUST NOT BE EMPTY")
+   @Pattern(regexp = "[a-zA-Z]*",message = "The keyword must contains only letters")
+   //@Email
+   //@Past
+   //@Size
    String keyword;
 }
