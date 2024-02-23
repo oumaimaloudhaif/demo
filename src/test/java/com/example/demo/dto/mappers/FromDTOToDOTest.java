97801f32d26811d5c61ee02f7f76b42c12cf165d
Oumaima <oumaymaloudhaif96@gmaol.com>
2024-02-23 15:21:51 +0100

    test: Enhance dtos tests

--- src/test/java/com/example/demo/dto/mappers/FromDTOToDOTest.java
+++ src/test/java/com/example/demo/dto/mappers/FromDTOToDOTest.java
@@ -3,22 +3,8 @@ package com.example.demo.dto.mappers;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.example.demo.DemoApplication;
-import com.example.demo.dto.AddressDTO;
-import com.example.demo.dto.CompanyDTO;
-import com.example.demo.dto.DepartmentDTO;
-import com.example.demo.dto.EmployeeDTO;
-import com.example.demo.dto.MeetingDTO;
-import com.example.demo.dto.ProjectDTO;
-import com.example.demo.dto.ReportDTO;
-import com.example.demo.dto.TaskDTO;
-import com.example.demo.entities.Address;
-import com.example.demo.entities.Company;
-import com.example.demo.entities.Department;
-import com.example.demo.entities.Employee;
-import com.example.demo.entities.Meeting;
-import com.example.demo.entities.Project;
-import com.example.demo.entities.Report;
-import com.example.demo.entities.Task;
+import com.example.demo.dto.*;
+import com.example.demo.entities.*;
 import com.example.demo.enums.ContractType;
 import com.example.demo.enums.Gender;
 import com.example.demo.enums.Priority;
@@ -42,7 +28,7 @@ class FromDTOToDOTest {
   void mapAddressDTO() {
     final AddressDTO addressDto = new AddressDTO("Street", "City", "7034");
 
-    final Address result = mapper.MapAddressDTO(addressDto);
+    final Address result = mapper.mapAddressDTO(addressDto);
 
     assertEquals("Street", result.getStreet());
     assertEquals("City", result.getCity());
@@ -53,7 +39,7 @@ class FromDTOToDOTest {
   void mapCompanyDTO() {
     final CompanyDTO companyDTO = new CompanyDTO("Company Name");
 
-    final Company result = mapper.MapCompanyDTO(companyDTO);
+    final Company result = mapper.mapCompanyDTO(companyDTO);
 
     assertEquals("Company Name", result.getName());
   }
@@ -64,7 +50,7 @@ class FromDTOToDOTest {
     final List<Employee> employees = List.of(employee);
     final DepartmentDTO departmentDTO = new DepartmentDTO("Info", employees);
 
-    final Department result = mapper.MapDepartmentDTO(departmentDTO);
+    final Department result = mapper.mapDepartmentDTO(departmentDTO);
 
     assertEquals("Info", result.getName());
     assertEquals(employees.size(), result.getEmployees().size());
@@ -75,7 +61,7 @@ class FromDTOToDOTest {
     final EmployeeDTO employeeDTO =
         new EmployeeDTO("Oumaima L", 1000, Gender.FEMALE, ContractType.CDI);
 
-    final Employee result = mapper.MapEmployeeDTO(employeeDTO);
+    final Employee result = mapper.mapEmployeeDTO(employeeDTO);
 
     assertEquals("Oumaima L", result.getName());
     assertEquals(Gender.FEMALE, result.getGender());
@@ -87,7 +73,7 @@ class FromDTOToDOTest {
   void mapMeetingDTO() {
     final MeetingDTO meetingDTO = new MeetingDTO("meeting");
 
-    final Meeting result = mapper.MapMeetingDTO(meetingDTO);
+    final Meeting result = mapper.mapMeetingDTO(meetingDTO);
 
     assertEquals("meeting", result.getTitle());
   }
@@ -96,7 +82,7 @@ class FromDTOToDOTest {
   void mapProject() {
     final ProjectDTO projectDTO = new ProjectDTO("project");
 
-    final Project result = mapper.MapProjectDTO(projectDTO);
+    final Project result = mapper.mapProjectDTO(projectDTO);
 
     assertEquals("project", result.getName());
   }
@@ -105,7 +91,7 @@ class FromDTOToDOTest {
   void mapReport() {
     final ReportDTO reportDTO = new ReportDTO("report");
 
-    final Report result = mapper.MapReport(reportDTO);
+    final Report result = mapper.mapReport(reportDTO);
 
     assertEquals("report", result.getTitle());
   }
@@ -115,11 +101,22 @@ class FromDTOToDOTest {
     final TaskDTO taskDTO =
         new TaskDTO("task", "description", Priority.HIGH, TaskStatus.IN_PROGRESS);
 
-    final Task result = mapper.MapTask(taskDTO);
+    final Task result = mapper.mapTask(taskDTO);
 
     assertEquals("task", result.getName());
     assertEquals("description", result.getDescription());
     assertEquals(TaskStatus.IN_PROGRESS, result.getTaskStatus());
     assertEquals(Priority.HIGH, result.getPriority());
+  }
+
+  @Test
+  void mapWorkCalendar() {
+    final WorkCalendarDTO workCalendarDTO =
+            new WorkCalendarDTO("tag");
+
+    final WorkCalendar result = mapper.mapWorkCalendar(workCalendarDTO);
+
+    assertEquals("tag", result.getTag());
+
   }
 }
