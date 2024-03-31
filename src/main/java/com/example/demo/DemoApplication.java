40ea3662d687578dc6489e9229e618a8ca047cf1
Oumaima <oumaymaloudhaif96@gmaol.com>
2024-03-31 23:19:20 +0100

    refactor: Remove environment variables

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import com.example.demo.config.SecretConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,7 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class DemoApplication {
   public static void main(String[] args) {
-    SecretConfig.loadEnv();
     SpringApplication.run(DemoApplication.class, args);
   }
 }
